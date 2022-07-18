51aea68198a61263447afe0cb6552003c650ef4b
Túlio Bernardino <94199063+t-bernardino@users.noreply.github.com>
2022-07-17 23:45:33 -0300

    Update interfaceBD.py
    alteracoes na interface

--- interfaceBD.py
+++ interfaceBD.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import Toplevel, ttk
 
 class InterfaceGrafica():
 
@@ -7,7 +7,7 @@ class InterfaceGrafica():
           
     def criaJanelaEstrutura(janela):
         janelaEstrutura = tk.Toplevel(janela)
-        frameJanelaEstrutura = tk.Frame(master=janelaEstrutura, width=800, height=200, bg="yellow")
+        frameJanelaEstrutura = tk.Frame(master=janelaEstrutura, width=900, height=200, bg="yellow")
         
         #FUNÇÃO DEVE CRIAR BASE E RETORNAR MENSAGEM DE SUCESSO OU ERRO
 
@@ -19,6 +19,8 @@ class InterfaceGrafica():
         botaoSair.place(x=200,y=150)
         frameJanelaEstrutura.pack()
 
+
+    """
     def criaLayoutPesquisa(janela,opcao):
 
         match opcao:
@@ -94,7 +96,14 @@ class InterfaceGrafica():
 
                 mensagemInvalida = tk.Label(janela,text="OPCÃO INVÁLIDA")
                 mensagemInvalida.pack()
-            
+            """
+    
+    def criaJanelaResultado(janela):
+
+        janelaResultado = Toplevel(janela)
+        frameJanelaResultado = tk.Frame(master=janelaResultado, width=600, height=600, background="white")
+
+        
     
     def criaJanelaPesquisa(janela):
         
@@ -127,6 +136,8 @@ class InterfaceGrafica():
         abas.add(abaCliente,text="Cliente")
         abas.add(abaProfissional,text="Profissional")
         abas.add(abaJob,text="Job")
+
+        #ELEMENTOS DA ABA CLIENTE
 
         rotuloCPF = tk.Label(abaCliente,text="CPF")
         rotuloCPF.place(x=20,y=25)
@@ -166,6 +177,11 @@ class InterfaceGrafica():
         botaoPesquisar = tk.Button(master=abaCliente,text="PESQUISAR")
         botaoPesquisar.place(x=200,y=235)
 
+        botaoVoltar = tk.Button(master=abaCliente,text="VOLTAR",command=janelaPesquisa.destroy)
+        botaoVoltar.place(x=350,y=235)
+
+        #ELEMENTOS DA ABA PROFISSIONAL
+
         rotuloCPF = tk.Label(abaProfissional,text="CPF")
         rotuloCPF.place(x=20,y=25)
         campoCPF = tk.Text(master=abaProfissional, height=1,width=60)
@@ -204,8 +220,163 @@ class InterfaceGrafica():
         botaoPesquisar = tk.Button(master=abaProfissional,text="PESQUISAR")
         botaoPesquisar.place(x=200,y=235)
 
+        botaoVoltar = tk.Button(master=abaProfissional,text="VOLTAR",command=janelaPesquisa.destroy)
+        botaoVoltar.place(x=350,y=235)
+
+        #ELEMENTOS DA ABA JOB
+
+        rotuloClienteJob = tk.Label(abaJob,text="Cliente")
+        rotuloClienteJob.place(x=20,y=25)
+        campoClienteJob = tk.Text(master=abaJob, height=1,width=60)
+        campoClienteJob.place(x=100,y=25)
+                
+        rotuloTituloJob = tk.Label(abaJob,text="Título:")
+        rotuloTituloJob.place(x=20,y=55)
+        campoTituloJob = tk.Text(master=abaJob,height=1,width=60)
+        campoTituloJob.place(x=100,y=55)
+
+        rotuloDescricaoJob = tk.Label(abaJob,text="Descrição:")
+        rotuloDescricaoJob.place(x=20,y=85)
+        campoDescricaoJob = tk.Text(master=abaJob,height=1,width=60)
+        campoDescricaoJob.place(x=100,y=85)
+
+        botaoPesquisar = tk.Button(master=abaJob,text="PESQUISAR")
+        botaoPesquisar.place(x=200,y=115)
+
+        botaoVoltar = tk.Button(master=abaJob,text="VOLTAR",command=janelaPesquisa.destroy)
+        botaoVoltar.place(x=350,y=115)
+
         abas.pack()
         frameJanelaPesquisa.pack()
+
+    def criaJanelaInsercao(janela):
+
+        janelaInsercao = tk.Toplevel(janela)
+        frameJanelaInsercao = tk.Frame(master=janelaInsercao, width=900, height=800, bg="yellow")
+
+        abas = ttk.Notebook(frameJanelaInsercao)
+        abaCliente = tk.Frame(abas,width=900,height=800)
+        abaProfissional = tk.Frame(abas,width=900,height=800)
+        abaJob = tk.Frame(abas,width=900,height=800)
+
+        abas.add(abaCliente,text="Cliente")
+        abas.add(abaProfissional,text="Profissional")
+        abas.add(abaJob,text="Job")
+
+        #ELEMENTOS DA ABA CLIENTE
+
+        rotuloCPF = tk.Label(abaCliente,text="CPF")
+        rotuloCPF.place(x=20,y=25)
+        campoCPF = tk.Text(master=abaCliente, height=1,width=60)
+        campoCPF.place(x=100,y=25)
+                
+        rotuloNome = tk.Label(abaCliente,text="Nome:")
+        rotuloNome.place(x=20,y=55)
+        campoNome = tk.Text(master=abaCliente,height=1,width=60)
+        campoNome.place(x=100,y=55)
+
+        rotuloEmail = tk.Label(abaCliente,text="E-mail:")
+        rotuloEmail.place(x=20,y=85)
+        campoEmail = tk.Text(master=abaCliente,height=1,width=60)
+        campoEmail.place(x=100,y=85)
+
+        rotuloTelefone = tk.Label(abaCliente,text="Telefone:")
+        rotuloTelefone.place(x=20,y=115)
+        campoTelefone = tk.Text(master=abaCliente,height=1,width=60)
+        campoTelefone.place(x=100,y=115)
+
+        rotuloRua = tk.Label(abaCliente,text="Rua")
+        rotuloRua.place(x=20,y=145)
+        campoRua = tk.Text(master=abaCliente, height=1,width=60)
+        campoRua.place(x=100,y=145)
+                
+        rotuloNumero = tk.Label(abaCliente,text="Numero:")
+        rotuloNumero.place(x=20,y=175)
+        campoNumero = tk.Text(master=abaCliente,height=1,width=60)
+        campoNumero.place(x=100,y=175)
+
+        rotuloCEP = tk.Label(abaCliente,text="CEP:")
+        rotuloCEP.place(x=20,y=205)
+        campoCEP = tk.Text(master=abaCliente,height=1,width=60)
+        campoCEP.place(x=100,y=205)
+
+        botaoPesquisar = tk.Button(master=abaCliente,text="CADASTRAR")
+        botaoPesquisar.place(x=200,y=235)
+
+        botaoVoltar = tk.Button(master=abaCliente,text="VOLTAR",command=janelaInsercao.destroy)
+        botaoVoltar.place(x=350,y=235)
+
+        #ELEMENTOS DA ABA PROFISSIONAL
+
+        rotuloCPF = tk.Label(abaProfissional,text="CPF")
+        rotuloCPF.place(x=20,y=25)
+        campoCPF = tk.Text(master=abaProfissional, height=1,width=60)
+        campoCPF.place(x=100,y=25)
+                
+        rotuloNome = tk.Label(abaProfissional,text="Nome:")
+        rotuloNome.place(x=20,y=55)
+        campoNome = tk.Text(master=abaProfissional,height=1,width=60)
+        campoNome.place(x=100,y=55)
+
+        rotuloEmail = tk.Label(abaProfissional,text="E-mail:")
+        rotuloEmail.place(x=20,y=85)
+        campoEmail = tk.Text(master=abaProfissional,height=1,width=60)
+        campoEmail.place(x=100,y=85)
+
+        rotuloTelefone = tk.Label(abaProfissional,text="Telefone:")
+        rotuloTelefone.place(x=20,y=115)
+        campoTelefone = tk.Text(master=abaProfissional,height=1,width=60)
+        campoTelefone.place(x=100,y=115)
+
+        rotuloRua = tk.Label(abaProfissional,text="Rua")
+        rotuloRua.place(x=20,y=145)
+        campoRua = tk.Text(master=abaProfissional, height=1,width=60)
+        campoRua.place(x=100,y=145)
+                
+        rotuloNumero = tk.Label(abaProfissional,text="Numero:")
+        rotuloNumero.place(x=20,y=175)
+        campoNumero = tk.Text(master=abaProfissional,height=1,width=60)
+        campoNumero.place(x=100,y=175)
+
+        rotuloCEP = tk.Label(abaProfissional,text="CEP:")
+        rotuloCEP.place(x=20,y=205)
+        campoCEP = tk.Text(master=abaProfissional,height=1,width=60)
+        campoCEP.place(x=100,y=205)
+
+        botaoPesquisar = tk.Button(master=abaProfissional,text="CADASTRAR")
+        botaoPesquisar.place(x=200,y=235)
+
+        botaoVoltar = tk.Button(master=abaProfissional,text="VOLTAR",command=janelaInsercao.destroy)
+        botaoVoltar.place(x=350,y=235)
+
+        #ELEMENTOS DA ABA JOB
+
+        rotuloClienteJob = tk.Label(abaJob,text="Cliente")
+        rotuloClienteJob.place(x=20,y=25)
+        campoClienteJob = tk.Text(master=abaJob, height=1,width=60)
+        campoClienteJob.place(x=100,y=25)
+                
+        rotuloTituloJob = tk.Label(abaJob,text="Título:")
+        rotuloTituloJob.place(x=20,y=55)
+        campoTituloJob = tk.Text(master=abaJob,height=1,width=60)
+        campoTituloJob.place(x=100,y=55)
+
+        rotuloDescricaoJob = tk.Label(abaJob,text="Descrição:")
+        rotuloDescricaoJob.place(x=20,y=85)
+        campoDescricaoJob = tk.Text(master=abaJob,height=1,width=60)
+        campoDescricaoJob.place(x=100,y=85)
+
+        botaoPesquisar = tk.Button(master=abaJob,text="CADASTRAR")
+        botaoPesquisar.place(x=200,y=115)
+
+        botaoVoltar = tk.Button(master=abaJob,text="VOLTAR",command=janelaInsercao.destroy)
+        botaoVoltar.place(x=350,y=115)
+
+        abas.pack()
+        frameJanelaInsercao.pack()
+        
+
+
 
 
 
@@ -226,7 +397,7 @@ class InterfaceGrafica():
         #FAZER BIND AO MÉTODO DE PESQUISA
         botaoDois.place(x=10,y=120)
 
-        botaoTres = tk.Button(master=framePrincipal,text="CADASTRAR ENTRADA")
+        botaoTres = tk.Button(master=framePrincipal,text="CADASTRAR ENTRADA", command= lambda: InterfaceGrafica.criaJanelaInsercao(self.janela))
         #FAZER BIND AO MÉTODO QUE CADASTRA NA BASE
         botaoTres.place(x=10,y=180)
 
